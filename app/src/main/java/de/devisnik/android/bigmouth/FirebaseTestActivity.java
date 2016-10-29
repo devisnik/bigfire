9254388fc5afb69dc57fd09c73ca7391f31c8926
Volker Leck <volker.leck@gmail.com>
2016-10-29 11:53:09 +0100

    initial update and sync logic

--- app/src/main/java/de/devisnik/android/bigmouth/FirebaseTestActivity.java
+++ app/src/main/java/de/devisnik/android/bigmouth/FirebaseTestActivity.java
@@ -5,9 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.Date;
 
 public class FirebaseTestActivity extends AppCompatActivity {
+
+  private String TAG = getClass().getSimpleName();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,34 @@ public class FirebaseTestActivity extends AppCompatActivity {
             .setAction("Action", null).show();
       }
     });
-  }
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference myRef = database.getReference("test_sync_message");
+
+    Button button = (Button) findViewById(R.id.test_button_update);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        myRef.setValue(new Date().toString());
+      }
+    });
+
+    TextView value = (TextView) findViewById(R.id.test_value_text);
+
+    myRef.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        String value = dataSnapshot.getValue(String.class);
+        Log.d(TAG, "Value is: " + value);
+      }
+
+      @Override
+      public void onCancelled(DatabaseError error) {
+        // Failed to read value
+        Log.w(TAG, "Failed to read value.", error.toException());
+      }
+    });  }
 
 }
