96ee4bd5a3116d0d7d46bec7b74693815d9e6017
Tobias Heine <tobias@novoda.com>
2016-11-13 14:31:17 +0100

    Introduce EditChannelActivity skeleton

---
+++ app/src/main/java/de/devisnik/android/bigmouth/channels/EditChannelActivity.kt
@@ -0,0 +1,13 @@
+package de.devisnik.android.bigmouth.channels
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import de.devisnik.android.bigmouth.R
+
+class EditChannelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_channel)
+    }
+}
